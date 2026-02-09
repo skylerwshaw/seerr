2916803538531708729128c47dd87de520d149fe
Michael Thomas <michaelhthomas@outlook.com>
2026-02-09 00:17:02 -0500

    feat: support linking OpenID Connect accounts to users

--- server/interfaces/api/userSettingsInterfaces.ts
+++ server/interfaces/api/userSettingsInterfaces.ts
@@ -1,4 +1,7 @@
-import type { NotificationAgentKey } from '@server/lib/settings';
+import type {
+  NotificationAgentKey,
+  PublicOidcProvider,
+} from '@server/lib/settings';
 
 export interface UserSettingsGeneralResponse {
   username?: string;
@@ -38,3 +41,11 @@ export interface UserSettingsNotificationsResponse {
   webPushEnabled?: boolean;
   notificationTypes: Partial<NotificationAgentTypes>;
 }
+
+export type UserSettingsLinkedAccount = {
+  id: number;
+  username: string;
+  provider: PublicOidcProvider;
+};
+
+export type UserSettingsLinkedAccountResponse = UserSettingsLinkedAccount[];
